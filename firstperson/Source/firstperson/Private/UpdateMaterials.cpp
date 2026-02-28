1e2f755400f24ff3574c7cb3f523bb5ac897b9c8
Khoi Van <vanthekhoi@gmail.com>
2026-02-28 11:39:02 +0700

    [Assigment-6] Update Materials (MPC)

---
+++ firstperson/Source/firstperson/Private/UpdateMaterials.cpp
@@ -0,0 +1,47 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "UpdateMaterials.h"
+
+#include "Kismet/KismetMaterialLibrary.h"
+
+
+// Sets default values for this component's properties
+UUpdateMaterials::UUpdateMaterials()
+{
+	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
+	// off to improve performance if you don't need them.
+	PrimaryComponentTick.bCanEverTick = true;
+
+	// ...
+	ParameterName = "ColorMultiply";
+}
+
+
+// Called when the game starts
+void UUpdateMaterials::BeginPlay()
+{
+	Super::BeginPlay();
+
+	// ...
+	
+}
+
+
+// Called every frame
+void UUpdateMaterials::TickComponent(float DeltaTime, ELevelTick TickType,
+                                     FActorComponentTickFunction* ThisTickFunction)
+{
+	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+
+	// ...
+}
+
+void UUpdateMaterials::UpdateMaterialParameterCollection(FLinearColor ParameterValue)
+{
+	if (MaterialParameterCollection)
+	{
+		UKismetMaterialLibrary::SetVectorParameterValue(GetWorld(), MaterialParameterCollection, ParameterName, ParameterValue);
+	}
+}
+
---
+++ firstperson/Source/firstperson/Public/UpdateMaterials.h
@@ -0,0 +1,39 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/ActorComponent.h"
+#include "UpdateMaterials.generated.h"
+
+
+UCLASS(ClassGroup=(Custom), meta=(BlueprintSpawnableComponent))
+class FIRSTPERSON_API UUpdateMaterials : public UActorComponent
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this component's properties
+	UUpdateMaterials();
+
+protected:
+	// Called when the game starts
+	virtual void BeginPlay() override;
+
+public:
+	// Called every frame
+	virtual void TickComponent(float DeltaTime, ELevelTick TickType,
+	                           FActorComponentTickFunction* ThisTickFunction) override;
+	
+	// Setup Material Parameter Collection
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Material Parameter Collection")
+	class UMaterialParameterCollection* MaterialParameterCollection;
+	
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Material Parameter Collection")
+	FName ParameterName;
+	
+	
+	// Function to update the material parameter collection
+	UFUNCTION(BlueprintCallable, Category = "Material Parameter Collection")
+	void UpdateMaterialParameterCollection(FLinearColor ParameterValue);
+};
